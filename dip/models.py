7da1368737a5895e500a8c4c7498a581b116b804
sean-r-duffy <duffy.sean.14@gmail.com>
2025-05-02 15:40:59 -0400

    Moved code to dip/ package. Commented models.py and utils.py

---
+++ dip/models.py
@@ -0,0 +1,179 @@
+import torch.nn as nn
+from torch import Tensor
+
+
+def down_layer(in_channels: int, out_channels: int, k: int) -> nn.Sequential:
+    """
+    Constructs a down-sampling layer using convolution, batch normalization, and LeakyReLU activation operations.
+    The layer reduces spatial dimensions and increases the number of channels (feature maps).
+
+    :param in_channels: Number of input channels.
+    :param out_channels: Number of output channels.
+    :param k: Kernel size for the convolutional layers.
+
+    :return: A Sequential layer encapsulating the down-sampling operation.
+    """
+    layer = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=k, stride=2, padding=k // 2),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(out_channels, out_channels, kernel_size=k, stride=1, padding=k // 2),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(negative_slope=0.1)
+    )
+    return layer
+
+
+def up_layer(in_channels: int, out_channels: int, k: int, upsampling: str = 'bilinear') -> nn.Sequential:
+    """
+    Constructs an upsampling layer using transpose convolution, batch normalization, bilinear interpolation or
+    nearest neighbor upsampling, and LeakyReLU activation operations.
+    The layer increased spatial dimensions and decreases the number of channels (feature maps).
+
+    :param in_channels: The number of input channels.
+    :param out_channels: The number of output channels.
+    :param k: Kernel size for the transposed convolution operation.
+    :param upsampling: Defines the upsampling method. Accepted values are 'bilinear' or
+        'nearest'. Defaults to 'bilinear'.
+
+    :return: A Sequential layer encapsulating the up-sampling operation.
+    """
+    if upsampling == 'bilinear':
+        upsample_layer = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+    elif upsampling == 'nearest':
+        upsample_layer = nn.Upsample(scale_factor=2, mode='nearest')
+    else:
+        raise ValueError("Invalid upsampling method")
+
+    layer = nn.Sequential(
+        nn.BatchNorm2d(in_channels),
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=k, stride=1, padding=k // 2),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=1 // 2),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(negative_slope=0.1),
+        upsample_layer
+    )
+    return layer
+
+
+def skip_layer(in_channels: int, out_channels: int, k: int) -> nn.Sequential | None:
+    """
+    Creates a skip connection layer with a convolutional block, batch normalization, and LeakyReLU activation.
+    If `out_channels` is 0, the function returns None (there is no skip connection).
+    Kernal size k is usually 1, to preserve spatial dimensions.
+
+    :param in_channels: Number of input channels.
+    :param out_channels: Number of output channels; if set to 0, no layer is created.
+    :param k: Kernel size for the convolutional layer
+
+    :return: A Sequential layer encapsulating the skip-connection operation.
+    """
+    if out_channels != 0:
+        layer = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=k, stride=1, padding=k // 2),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.1)
+        )
+        return layer
+    else:
+        return None
+
+
+class DIPNet(nn.Module):
+    """
+    Defines a deep image prior network for image reconstruction, which consists
+    of an UNet-like encoder-decoder architecture with skip connections. The network supports
+    different upsampling methods and generates three-channel output images.
+
+    :ivar n: Number of layers in the encoder/decoder.
+    :ivar c: Number of input channels in the input tensor.
+    :ivar encoder: Module list comprising the downsampling (encoder) layers.
+    :ivar decoder: Module list comprising the upsampling (decoder) layers.
+    :ivar skip_connections: Module list comprising the layers skip connection layers.
+    :ivar final_layer: Convolutional layer to produce the three-channel output image.
+    """
+
+    def __init__(self, nu: list[int], ku: list[int], nd: list[int], kd: list[int], ns: list[int], ks: list[int],
+                 d_in: tuple, upsampling: str = 'bilinear') -> None:
+        """
+        Initializes the DIPNet class with encoder, decoder, and skip connection modules.
+
+        :param nu: List of integers specifying the output channel dimensions used in the up-sampling (decoder) layers.
+        :param ku: List of integers specifying the kernel sizes used by the up-sampling (decoder) layers.
+        :param nd: List of integers specifying the output channel dimensions used in the down-sampling (encoder) layers.
+        :param kd: List of integers specifying the kernel sizes used by the down-sampling (encoder) layers.
+        :param ns: List of integers specifying the output channel dimensions used in the skip connection layers.
+            Set a value of 0 for layers without skip connections.
+        :param ks: List of integers specifying the kernel sizes used by the skip connection layers.
+            Should usually be 1, to preserve spatial dimensions.
+        :param d_in: A tuple representing the dimensions of the input data; formatted as (channels, height, width).
+        :param upsampling: A string specifying the up-sampling method, either 'bilinear' or 'nearest'.
+            Defaults to 'bilinear'.
+        """
+        super(DIPNet, self).__init__()
+
+        # Confirm there are an equal number of encoder, decoder, and skip connection layers
+        assert len(nu) == len(ku) == len(ns) == len(ks) == len(nd) == len(kd)
+        self.n = len(nu)  # Depth of the network
+        self.c = d_in[1]  # Channels in the input tensor
+
+        encoder_layers = []
+        decoder_layers = []
+        skip_layers = []
+
+        # Initialize 'top' encoder and skip connections layers separately from 'top' decoder
+        encoder_layers.append(down_layer(self.c, nd[0], kd[0]))
+        skip_layers.append(skip_layer(nd[0], ns[0], ks[0]))
+
+        # Construct 'middle' layers
+        for i in range(1, self.n):
+            encoder_layers.append(down_layer(nd[i - 1], nd[i], kd[i]))
+            skip_layers.append(skip_layer(nd[i], ns[i], ks[i]))
+        for i in range(self.n - 1):
+            decoder_layers.append(up_layer(nd[i + 1], nd[i], ku[i]))
+
+        # Initialize 'top' decode layer separately
+        decoder_layers.append(up_layer(nu[self.n - 1], nu[self.n - 1], ku[self.n - 1]))
+
+        # Correct order
+        decoder_layers.reverse()
+        skip_layers.reverse()
+
+        self.encoder = nn.ModuleList(encoder_layers)
+        self.decoder = nn.ModuleList(decoder_layers)
+        self.skip_connections = nn.ModuleList(skip_layers)
+        self.final_layer = nn.Conv2d(nu[0], 3, kernel_size=1, stride=1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+            Processes the input tensor through the network.
+
+            :param x: Input tensor to be passed through the network.
+            :return: Output tensor after processing through the network.
+            """
+
+        # Run through encoder and store state after each layer for use in skip connections
+        skip_outs = [None]
+        for layer in self.encoder:
+            x = layer(x)
+            skip_outs.append(x)
+        skip_outs = skip_outs[:-1]
+
+        # Run through decoder, adding skip connection layer output (if present) after each decoder layer
+        for idx, layer in enumerate(self.decoder):
+            x = layer(x)
+            if self.skip_connections[idx] and skip_outs[-(idx + 1)] is not None:
+                skip = self.skip_connections[idx](skip_outs[-(idx + 1)])
+
+                # Repeat skip connection until channel number matches decoder output channel number
+                repeat_factor = x.shape[1] // skip.shape[1]
+                skip = skip.repeat(1, repeat_factor, 1, 1)
+
+                # Sum decoder output and skip connection output
+                x += skip
+
+        x = self.final_layer(x)
+
+        return x
---
+++ dip/utils.py
@@ -0,0 +1,92 @@
+import torch
+from matplotlib import pyplot as plt
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
+
+
+def load_image(path: str, target_size: tuple[int, int] = None, tensor: bool = True) -> Image or torch.Tensor:
+    """
+    Loads an image from a specified path, processes it as RGB, and optionally resizes.
+
+    :param path: Path to the image file to be loaded.
+    :param target_size: Tuple specifying the desired image size (width, height). If
+        None, the image is not resized.
+    :param tensor: If True, the processed image is converted to a PyTorch Tensor with normalized values
+        in the range [0, 1]. If False, the function returns the image as a PIL Image object.
+
+    :return: The processed image as a PIL Image or PyTorch Tensor.
+    """
+    image = Image.open(path).convert("RGB")
+    if target_size:
+        image = image.resize(target_size)
+    if tensor:
+        return ToTensor()(image).clamp(0, 1)
+    else:
+        return image
+
+
+def save_image(tensor: torch.Tensor, path: str) -> None:
+    """
+    Save a tensor as an image to a specified path on disk.
+
+    :param tensor: A PyTorch tensor representing the image to save. Formatted as (channels, height, width).
+    :param path: A string specifying the file path where the image should be saved and filename (with extension).
+
+    :return: None
+    """
+    image = ToPILImage()(tensor.squeeze(0).clamp(0, 1))
+    image.save(path)
+
+
+def add_noise(img: torch.Tensor, noise_factor: float = 0.1) -> torch.Tensor:
+    """
+    Adds random noise to an input image tensor by adding Gaussian noise and clamps
+    the resulting values between 0 and 1.
+
+    :param img: The input image represented as a torch.Tensor.
+    :param noise_factor: A float that scales the magnitude of Gaussian noise added
+        to the input image. Default is 0.1.
+
+    :return: A new torch.Tensor with added Gaussian noise, normalized to the range [0, 1].
+    """
+    noisy_img = img + noise_factor * torch.randn_like(img)
+    return noisy_img.clamp(0, 1).detach()
+
+
+def plot_image(tensor: torch.Tensor, title: str = None) -> None:
+    """
+    Plots a single image tensor using Matplotlib.
+
+    :param tensor: PyTorch tensor representing the image to be plotted.
+    :param title: Optional title to be displayed above the image. Defaults to None.
+
+    :return: None; displays the image.
+    """
+    image = ToPILImage()(tensor.squeeze(0))
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+
+def plot_images(tensors: list, titles: list = None) -> None:
+    """
+    Displays a sequence of tensors as images in a single row with optional titles.
+
+    :param tensors: A list of images (as tensors) to be displayed.
+    :param titles: An optional list of strings containing titles for each
+        image. Default is None.
+
+    :return: None; displays the images.
+    """
+    num_images = len(tensors)
+    plt.figure(figsize=(5 * num_images, 5))
+    for i, tensor in enumerate(tensors):
+        image = ToPILImage()(tensor.squeeze())
+        plt.subplot(1, num_images, i + 1)
+        plt.imshow(image)
+        if titles and i < len(titles):
+            plt.title(titles[i])
+        plt.axis('off')
+    plt.show()
